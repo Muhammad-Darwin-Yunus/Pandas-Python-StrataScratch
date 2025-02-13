18897d089340ea61a64aafda29f90d9e33cf456d
Muhammad-Darwin-Yunus <muhdarwinyunus02@gmail.com>
2025-02-13 12:45:54 +0700

    Create ID 10168.py

---
+++ Code Pandas/ID 10168.py
@@ -0,0 +1,38 @@
+>.<
+
+Title: Number Of Records By Variety
+Link: https://platform.stratascratch.com/coding/10168-number-of-records-by-variety?code_type=2
+Level: Easy
+
+Find the total number of records that belong to each variety in the dataset.
+Output the variety along with the corresponding number of records.
+Order records by the variety in ascending order.
+
+Data iris:
+sepal_length: float
+sepal_width: float
+petal_length: float
+petal_width: float
+variety: varchar
+
+>.<
+
+Solution:
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+
+number_variety = pd.DataFrame(iris)
+
+number_variety = number_variety.groupby('variety')['sepal_length'].count().reset_index()
+
+number_variety = number_variety.rename(columns={'sepal_length':'total_variety'})
+
+Output:
+
+variety | records
+Setosa      50
+Versicolor  50
+Virginica    50
