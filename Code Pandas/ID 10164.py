032ea8a245820ed31e638728263f4cb36a4259bc
Muhammad-Darwin-Yunus <muhdarwinyunus02@gmail.com>
2025-02-14 14:13:40 +0700

    Create ID 10164.py

---
+++ Code Pandas/ID 10164.py
@@ -0,0 +1,34 @@
+>.<
+
+Title: Total AdWords Earnings
+Link: https://platform.stratascratch.com/coding/10164-total-adwords-earnings?code_type=2
+Level: Easy
+
+Find the total AdWords earnings for each business type.
+Output the business types along with the total earnings.
+
+Data google_adwords_earnings:
+business_type: varchar
+n_employees: int
+year: int
+adwords_earnings: int
+
+>.<
+
+Solution:
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+
+adwords_earning = pd.DataFrame(google_adwords_earnings)
+
+adwords_earning = adwords_earning.groupby('business_type')['adwords_earnings'].sum().reset_index()
+
+Output:
+
+business_type | adwords_earnings
+media              247914579
+transport          132323280
+handyman            6042187
