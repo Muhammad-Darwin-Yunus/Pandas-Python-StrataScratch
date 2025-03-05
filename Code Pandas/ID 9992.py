275a529513ab9a5c2de98389007fc8cabd6e7ccb
Muhammad-Darwin-Yunus <muhdarwinyunus02@gmail.com>
2025-03-05 22:29:32 +0700

    Update ID 9992.py

--- Code Pandas/ID 9992.py
+++ Code Pandas/ID 9992.py
@@ -1,6 +1,6 @@
 >.<
 
-Title: Find how many times each artist appeared on the Spotify ranking list
+Title: Artist Appearance Count
 Link: https://platform.stratascratch.com/coding/9992-find-artists-that-have-been-on-spotify-the-most-number-of-times?code_type=2
 Level: Easy
 
